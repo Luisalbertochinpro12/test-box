d139909edf88f3111ef55ced0f0c40435d61a0ad
Luis Alberto <92180328+Luisalbertochinpro12@users.noreply.github.com>
2025-11-14 20:13:35 -0600

    Update test_validaciones_caja_blanca.py

--- Practicas 03 LAGO/test_validaciones_caja_blanca.py
+++ Practicas 03 LAGO/test_validaciones_caja_blanca.py
@@ -1,9 +1,5 @@
 from validaciones import validar_contrasena, validar_correo
 
-# --------------------------
-# PRUEBAS CAJA BLANCA CONTRASEÑA
-# --------------------------
-
 def test_contra_none():
     assert validar_contrasena(None, "luis") == False
 
@@ -29,10 +25,6 @@ def test_contra_valida():
     assert validar_contrasena("LuisPassword1@", "luis") == True
 
 
-# --------------------------
-# PRUEBAS CAJA BLANCA CORREO
-# --------------------------
-
 def test_correo_none():
     assert validar_correo(None) == False
 
@@ -40,10 +32,11 @@ def test_correo_sin_arroba():
     assert validar_correo("usuario.gmail.com") == False
 
 def test_correo_usuario_corto():
-    assert validar_correo("luis@correo.com") == False  # "luis" = 4 chars
+    assert validar_correo("luis@correo.com") == False 
 
 def test_correo_sin_dominio():
     assert validar_correo("usuario@correo") == False
 
 def test_correo_valido():
     assert validar_correo("usuario123@correo.com") == True
+
