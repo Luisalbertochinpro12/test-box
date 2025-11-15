78a5e0bed4fea2f878445a2cec3c97ce4e8dd4d6
Luis Alberto <92180328+Luisalbertochinpro12@users.noreply.github.com>
2025-11-14 20:13:53 -0600

    Update test_validaciones_caja_negra.py

--- Practicas 03 LAGO/test_validaciones_caja_negra.py
+++ Practicas 03 LAGO/test_validaciones_caja_negra.py
@@ -1,10 +1,6 @@
 import pytest
 from validaciones import validar_contrasena, validar_correo
 
-# -------------------------
-# PARTICIÓN DE EQUIVALENCIA
-# -------------------------
-
 @pytest.mark.parametrize("contra,usuario,resultado", [
     ("Usuario123!", "Usuario", True),   # válida
     ("abc", "user", False),             # longitud inválida
@@ -17,16 +13,10 @@ def test_contra_equivalencia(contra, usuario, resultado):
     assert validar_contrasena(contra, usuario) == resultado
 
 
-# -------------------------
-# VALORES LÍMITE CONTRASEÑA
-# -------------------------
 def test_contra_min_long_exacta():
     assert validar_contrasena("Aa1!user", "user") == True  # 8 exactos
 
 
-# -------------------------
-# EMAIL
-# -------------------------
 @pytest.mark.parametrize("correo,resultado", [
     ("usuario123@dominio.com", True),  # válido
     ("abcde@dom.com", False),          # usuario límite <6
@@ -35,3 +25,4 @@ def test_contra_min_long_exacta():
 ])
 def test_correo_equivalencia(correo, resultado):
     assert validar_correo(correo) == resultado
+
